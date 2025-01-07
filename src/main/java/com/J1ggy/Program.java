be7e885eb8baef7c01811addbdf384fc8569b394
Marztor <mhairi.torrance@outlook.com>
2025-01-07 11:46:56 +0000

    Setuo pom.xml and folder structure

---
+++ src/main/java/com/J1ggy/Program.java
@@ -0,0 +1,10 @@
+package com.J1ggy;
+public class Program{
+
+    public static void main(String[] args) {
+        
+        System.out.println("This is a Maven project in the package com.Jiggy");
+
+    }
+
+}
